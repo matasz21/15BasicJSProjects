e16fdb43d176d69cfbf57a3b986689e24e351cc5
Matasz21 <matas.sabaliauskas@gmail.com>
2022-09-03 14:37:24 +0300

    new file:   12 project/app.js 	new file:   12 project/gift.jpeg 	new file:   12 project/index.html 	new file:   12 project/logo.svg 	new file:   12 project/styles.css 	new file:   javascript-basic-projects-master.zip

---
+++ 12 project/app.js
@@ -0,0 +1,41 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date();
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const monthday = futureDate.getDate();
+let weekday = futureDate.getUTCDay();
+weekday = weekdays[weekday];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${monthday} ${month} ${year} ${hours}:${minutes}.`
